fd72b017b5f8086ed538880cb6d35bbf006914f1
Saurav Vishal <sauravm807@gmail.com>
2021-03-26 21:57:28 +0530

    new lines added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 "use strict";
+let isShowInputDiv = true;
+function init() {
+    setRandomBodyColor();
+    toggleInputOutputDiv();
+}
 
 document.querySelector('#submitBtn').addEventListener('click', function () {
+    setRandomBodyColor();
     var userinput = document.querySelector('#userinput');
 
     let string = userinput.value.trim().split(' ').sort();
@@ -25,4 +31,36 @@ document.querySelector('#submitBtn').addEventListener('click', function () {
 
     }
     showresult();
+    toggleInputOutputDiv();
+});
+
+function setRandomBodyColor() {
+    let r = Math.floor(Math.random() * (230 - 130) + 130);
+    let g = Math.floor(Math.random() * (230 - 130) + 130);
+    let b = Math.floor(Math.random() * (230 - 130) + 130);
+    let newColor = `rgb(${r},${g},${b})`
+    document.body.style.backgroundColor = newColor;
+}
+
+function showInputDiv() {
+    document.querySelector('#inputDiv').style.display = 'block';
+    document.querySelector('#outputDiv').style.display = 'none';
+}
+function showOutputDiv() {
+    document.querySelector('#inputDiv').style.display = 'none';
+    document.querySelector('#outputDiv').style.display = 'block';
+}
+
+function toggleInputOutputDiv() {
+    if(isShowInputDiv) {
+        showInputDiv();
+    }
+    if(!isShowInputDiv) {
+        showOutputDiv();
+    }
+    isShowInputDiv = !isShowInputDiv;
+}
+
+document.querySelector('#backBtn').addEventListener('click', function () {
+    toggleInputOutputDiv();
 });
